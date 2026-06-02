02e53908361d76a19b6899401f1aec264de89e0d
Milana Tosic <tosic2504@gmail.com>
2026-06-02 21:26:39 +0200

    Downloaded data

---
+++ Inteligentni-sistemi-4/README.dataset.txt
@@ -0,0 +1,6 @@
+# Inteligentni sistemi > 2026-06-02 9:17pm
+https://universe.roboflow.com/milanas-workspace-kbobq/inteligentni-sistemi-qpjez
+
+Provided by a Roboflow user
+License: CC BY 4.0
+
--- src/download_data.py
+++ src/download_data.py
@@ -6,6 +6,6 @@ rf = Roboflow(api_key="f53u4350cMewtGprlCIA")
 
 # Učitavanje projekta i verzije u kojoj su sve slike u Train skupu
 project = rf.project("inteligentni-sistemi-qpjez")
-dataset = project.version(1).download("yolov8")
+dataset = project.version(4).download("yolov8")
 
 print("Dataset je uspešno preuzet u koren projekta")
